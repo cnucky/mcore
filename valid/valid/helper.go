ece5447dd5c7917cfc5c1baeb8a7f9249dffedc3
chris <chris@xsnews.nl>
2015-05-15 15:10:52 +0200

    Added validate function that parses structs and slices recursively.

--- valid/valid/helper.go
+++ valid/valid/helper.go
@@ -1,6 +1,8 @@
 package valid
 
 import (
+	"fmt"
+	"reflect"
 	"strconv"
 )
 
@@ -19,4 +21,51 @@ func FnGetStr(i interface{}) (string, error) {
 
 func FnGetStrSlice(i interface{}) ([]string, error) {
 	return i.([]string), nil
+}
+
+/* Reflect struct */
+func Validate(t interface{}) bool {
+	/* Loop through each field in given struct */
+	s := reflect.Indirect(reflect.ValueOf(t))
+	for num := 0; num < s.NumField(); num++ {
+		name := s.Type().Field(num).Name
+		if name == "_" {
+			/* Skip unexported fields */
+			continue
+		}
+
+		/* Exported field, specific field rule */
+		value := s.Field(num).Interface()
+
+		/* Get validation rule */
+		rule := s.Type().Field(num).Tag.Get("validate")
+		if len(rule) == 0 {
+			continue
+		}
+
+		/* Create parser for this rule and pass the context to it */
+		l := new(Valdsl)
+		l.Debug = true
+		err := l.Parse(t, rule, value)
+		if err != nil {
+			/* Deverror in rule */
+			panic(err)
+		}
+
+		/* Is this a slice? */
+		if s.Type().Field(num).Type.Kind() == reflect.Slice {
+			/* Loop through slice and validate each item */
+			s := reflect.ValueOf(s.Field(num).Interface())
+			for i := 0; i < s.Len(); i++ {
+				ret := Validate(s.Index(i).Interface())
+				fmt.Println("ret [slice]:", ret)
+			}
+		} else if s.Type().Field(num).Type.Kind() == reflect.Struct {
+			/* If it's a struct, only validate the struct */
+			ret := Validate(s.Field(num).Interface())
+			fmt.Println("ret [struct]:", ret)
+		}
+	}
+
+	return true
 }
