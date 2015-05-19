b295760600a2889ee3cf7a8cfb07aaacefbd9c20
root <root@localhost.localdomain>
2015-05-19 14:26:34 +0200

    Refactor. Same memory was overwritten with every file read, refactored to return map[filename]=path

--- config/recurse.go
+++ config/recurse.go
@@ -6,28 +6,22 @@ import (
 	"github.com/xsnews/mcore/log"
 )
 
-// Parse JSON's recursively in given dir.
-func LoadDir(basedir string, retType interface{}) ([]interface{}, error) {
-	var out []interface{}
-
+func Files(basedir string) (map[string]string, error) {
+	out := make(map[string]string)
 	err := filepath.Walk(basedir, func(path string, f os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-		if !f.IsDir() {
-			// Only read dirs
+		if f.IsDir() {
+			// Only read files
 			return nil
 		}
 		if basedir == path {
 			// Ignore
 			return nil
 		}
-		log.Debug("Load config=%s", path)
-
-		if e := Load(path, &retType); e != nil {
-			return e
-		}
-		out = append(out, retType)
+		log.Debug("Found config=%s", path)
+		out[f.Name()] = path
 		return nil
 	})
 
