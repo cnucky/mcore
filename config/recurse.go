f7481beb68d3a14dbee09827b1e51418e08b3a5b
chris <chris@xsnews.nl>
2015-05-22 18:28:12 +0200

    Added recursive json config loader.

--- config/recurse.go
+++ config/recurse.go
@@ -1,9 +1,12 @@
 package config
 
 import (
-	"path/filepath"
-	"os"
+	"encoding/json"
+	"fmt"
 	"github.com/xsnews/mcore/log"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
 func Files(basedir string) (map[string]string, error) {
@@ -26,4 +29,47 @@ func Files(basedir string) (map[string]string, error) {
 	})
 
 	return out, err
+}
+
+func Json(basedir string, x interface{}) error {
+	files, err := Files(basedir)
+	if err != nil {
+		panic(err)
+	}
+
+	// Read content from all files
+	data := make(map[string]string)
+	for x, y := range files {
+		fh, err := os.Open(y)
+		if err != nil {
+			panic(err)
+		}
+		defer fh.Close()
+		content, err := ioutil.ReadFile(y)
+		if err != nil {
+			panic(err)
+		}
+		data[x] = string(content)
+	}
+
+	// No files found
+	if len(data) == 0 {
+		return nil
+	}
+
+	// Create k:v json from files, refactor this to a stream
+	content := "{\n"
+	for fn, c := range data {
+		content = content + fmt.Sprintf("\"%s\": %s,", fn, c)
+	}
+	content = content[0 : len(content)-1] // Remove trailing ,
+	content = content + "}\n"
+
+	// Unmarshal json
+	err = json.Unmarshal([]byte(content), &x)
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
 }
