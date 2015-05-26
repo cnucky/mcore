742f6a81cce2fe523c97f4fdc2f19d6b4676dbee
chris <chris@xsnews.nl>
2015-05-26 13:04:27 +0200

    Panic -> return err, removed unneeded for loop, changed variables.

--- config/recurse.go
+++ config/recurse.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/xsnews/mcore/log"
 	"io/ioutil"
@@ -14,7 +15,7 @@ var (
 	Extension = ".json"
 )
 
-func Files(basedir string) (map[string]string, error) {
+func files(basedir string) (map[string]string, error) {
 	out := make(map[string]string)
 	err := filepath.Walk(basedir, func(path string, f os.FileInfo, err error) error {
 		if err != nil {
@@ -37,60 +38,52 @@ func Files(basedir string) (map[string]string, error) {
 }
 
 func LoadJsonD(basedir string, x interface{}) error {
-	files, err := Files(basedir)
+	// Get list of files
+	filelist, err := files(basedir)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
-	if len(files) == 0 {
+	// No files found
+	if len(filelist) == 0 {
 		return nil
-	}
-
-	// Read content from all files
-	data := make(map[string]string)
-	for x, y := range files {
-		fh, err := os.Open(y)
-		if err != nil {
-			panic(err)
-		}
-		defer fh.Close()
-		content, err := ioutil.ReadFile(y)
-		if err != nil {
-			panic(err)
-		}
-		data[x] = string(content)
 	}
 
 	// Create one big json string where every file is a key
 	jsonCollection := ""
-	i := 0
-	for fn, c := range data {
-		i++
+	for filename, fullpath := range filelist {
+		// Check if we can open file
+		fh, err := os.Open(fullpath)
+		if err != nil {
+			return err
+		}
+		defer fh.Close()
 
 		// Only load directory.d/file.Extension
-		s := strings.Split(fn, Extension)
+		s := strings.Split(filename, Extension)
 		if len(s) != 2 {
-			panic("found an invalid file in directory")
+			return errors.New(fmt.Sprintf("Invalid file '%s' present in config dir.", filename))
+		}
+
+		// Load content from file
+		data, err := ioutil.ReadFile(fullpath)
+		if err != nil {
+			return err
 		}
 
 		// Add to our json structure with key "filename"
-		jsonCollection = jsonCollection + fmt.Sprintf("\"%s\": %s", s[0], c)
-		if len(data) == i {
-			// We're done, don't add trailing comma
-			break
-		}
-
+		jsonCollection = jsonCollection + fmt.Sprintf(`"%s": %s`, s[0], data)
 		// Add trailing comma
-		jsonCollection = jsonCollection + ",\n"
+		jsonCollection = jsonCollection + ","
 	}
 
-	// Finish json structure
-	jsonCollection = "{\n" + jsonCollection + "}\n"
+	// Finish json structure, remove trailing comma
+	jsonCollection = "{\n" + jsonCollection[:len(jsonCollection)-1] + "}\n"
 
 	// Unmarshal json
 	err = json.Unmarshal([]byte(jsonCollection), &x)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	return nil
